c176873e4e28783aadfefe272b8b12a5e0caf5dc
Ionică Bizău <bizauionica@gmail.com>
2016-03-02 07:00:22 +0200

    Use pixel-class
    Use pixel-class to manage the pixel objects. This makes the life easier since it starts to standardize the things and keep them united across the projects.

--- example/index.js
+++ example/index.js
@@ -4,16 +4,15 @@ const lwipPixels = require("../lib")
 
 lwip.open(`${__dirname}/octocat.png`, (err, img) => {
     console.log(lwipPixels(img));
-    // [ [ { r: 0, g: 0, b: 0, a: 0 },
+    // [ [ PixelClass { r: 0, g: 0, b: 0, a: 0 },
+    //     PixelClass { r: 0, g: 0, b: 0, a: 0 },
+    //     PixelClass { r: 0, g: 0, b: 0, a: 0 },
     //     ...
-    //     { r: 0, g: 0, b: 0, a: 0 },
-    //     { r: 0, g: 0, b: 0, a: 0 },
-    //     { r: 0, g: 0, b: 0, a: 0 } ],
-    //   ...
-    //   [ { r: 0, g: 0, b: 0, a: 0 },
-    //     { r: 0, g: 0, b: 0, a: 0 },
+    //     PixelClass { r: 0, g: 0, b: 0, a: 0 } ],
+    //   [ PixelClass { r: 0, g: 0, b: 0, a: 0 },
     //     ...
-    //     { r: 255, g: 255, b: 255, a: 0 },
+    //     PixelClass { r: 0, g: 0, b: 0, a: 0.14 },
     //     ...
-    //     { r: 0, g: 0, b: 0, a: 0 } ] ]
+    //     PixelClass { r: 0, g: 0, b: 0, a: 0 } ],
+    //   ... ]
 });
