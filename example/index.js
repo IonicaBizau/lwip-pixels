fa1e0ce0a397d2c4828e17d32476f675ad737a89
napul205 <100463532+napul205@users.noreply.github.com>
2023-10-03 10:28:39 +0530

    Update index.js

--- example/index.js
+++ example/index.js
@@ -4,15 +4,5 @@ const lwipPixels = require("../lib")
 
 lwip.open(`${__dirname}/octocat.png`, (err, img) => {
     console.log(lwipPixels(img));
-    // [ [ PixelClass { r: 0, g: 0, b: 0, a: 0 },
-    //     PixelClass { r: 0, g: 0, b: 0, a: 0 },
-    //     PixelClass { r: 0, g: 0, b: 0, a: 0 },
-    //     ...
-    //     PixelClass { r: 0, g: 0, b: 0, a: 0 } ],
-    //   [ PixelClass { r: 0, g: 0, b: 0, a: 0 },
-    //     ...
-    //     PixelClass { r: 0, g: 0, b: 0, a: 0.14 },
-    //     ...
-    //     PixelClass { r: 0, g: 0, b: 0, a: 0 } ],
-    //   ... ]
+   
 });
